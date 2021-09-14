fac83e63ded418553ddd75b8a34bec98ac060c8b
xyuzuki <tliu4@albany.edu>
2021-09-13 20:51:44 -0400

    Update TheOffice.sql

--- TheOffice.sql
+++ TheOffice.sql
@@ -289,3 +289,58 @@ AND employee.branch_id = 2;
 SELECT * FROM branch;
 SELECT * FROM branch_supplier;
 
+CREATE TABLE trigger_test (
+     message VARCHAR(100)
+);
+
+DELIMITER $$ //* changed delimiter from ; to $$ */
+CREATE
+	TRIGGER my_trigger BEFORE INSERT
+    ON employee
+    FOR EACH ROW BEGIN
+		INSERT INTO trigger_test VALUES('added new employee');
+        END$$
+DELIMITER ;
+
+INSERT INTO employee
+VALUES(109, 'Oscar', 'Martinez', '1968-02-19', 'M', 69000, 106, 3);
+
+
+SELECT * FROM trigger_test;
+
+
+DELIMITER $$
+CREATE
+    TRIGGER my_trigger BEFORE INSERT
+    ON employee
+    FOR EACH ROW BEGIN
+        INSERT INTO trigger_test VALUES(NEW.first_name);
+    END$$
+DELIMITER ;
+
+INSERT INTO employee
+VALUES(110, 'Kevin', 'Malone', '1978-02-19', 'M', 69000, 106, 3);
+
+SELECT * FROM trigger_test;
+
+DROP TRIGGER my_trigger;
+
+DELIMITER $$
+CREATE
+    TRIGGER my_trigger BEFORE INSERT
+    ON employee
+    FOR EACH ROW BEGIN
+         IF NEW.sex = 'M' THEN
+               INSERT INTO trigger_test VALUES('added male employee');
+         ELSEIF NEW.sex = 'F' THEN
+               INSERT INTO trigger_test VALUES('added female');
+         ELSE
+               INSERT INTO trigger_test VALUES('added other employee');
+         END IF;
+    END$$
+DELIMITER ;
+
+INSERT INTO employee
+VALUES(111, 'Pam', 'Beesly', '1988-02-19', 'F', 69000, 106, 3);
+
+SELECT * FROM trigger_test;
